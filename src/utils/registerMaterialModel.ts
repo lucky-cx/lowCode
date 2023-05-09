6a7e421ccaa7b4d7af2a8a24c47d28122f6651aa
EXE0\chaoxing.lu <luchaoxing@exexm.com>
2023-05-09 15:12:39 +0800

    feat: 去掉一些打印语句

--- src/utils/registerMaterialModel.ts
+++ src/utils/registerMaterialModel.ts
@@ -1,10 +1,3 @@
-// /***
-//  *
-//  *  1、该文件需要注册，物料模型的 json 描述（是否可以换成 ts 文件返回 json 格式数据的形式）
-//  *  2、处理 模型定义种，key-val 的关系（包含复杂数据格式的嵌套，如数组和对象）
-//  *  3、将模型定义本身，放置到 store 中（之前是在全局挂载）
-//  */
-
 import appStore from "~/store";
 
 // 初始化组件初始数据
@@ -34,7 +27,7 @@ const materialJsonFields: Record<string, MateriaNode> = import.meta.glob(
   "~/config/materialModel/*.ts",
   { import: "default", eager: true },
 );
-window.console.log(" materialJsonFields-->", materialJsonFields);
+// window.console.log(" materialJsonFields-->", materialJsonFields);
 // 1、将模型的结构信息，存储到 store
 // 2、根据模型的结构信息，生成初始化数据，存储到 store ，对应给画布区域组件的属性设置
 export function registerComponentsSchema(): void {
@@ -51,8 +44,8 @@ export function registerComponentsSchema(): void {
     }
   }
 
-  window.console.log(" registerComponentsSchema store -->", appStore);
+  // window.console.log(" registerComponentsSchema store -->", appStore);
   appStore.material.setJsonFields(fields); // json 结构信息  --> store
   appStore.material.setMaterialInitData(initDatas); // 各个文件的初始化数据 --> store
-  window.console.log("initDatas -->", initDatas);
+  // window.console.log("initDatas -->", initDatas);
 }
